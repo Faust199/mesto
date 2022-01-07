48a70878889166786f338cf23a22420e695c212d
Nikita Polezhaev <polnikita199@yandex.ru>
2022-01-07 09:19:48 +0300

    webpack added

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import PopupWithForm from "../components/PopupWithForm.js";
 import FormValidator from "../components/FormValidator.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import UserInfo from "../components/UserInfo.js";
+import "./index.css";
 
 import {
     initialCards,
